fix errors in rnn calcualtion delta, the derivative computing of activator is wrong

chap06/Check.py
@@ -1,18 +1,53 @@
 #!/usr/bin/env python
 # -*- coding:utf8 -*-
 # Power by viekie. 2017-06-01 14:55:23
-import RecurrentLayer
-import IdentityActivator
-import numpy
-
+from RecurrentLayer import RecurrentLayer
+from IdentityActivator import IdentityActivator
+import numpy as np
 
 def gradient_check():
-    error_function = lambda o: o.sum()
-    rl = RecurrentLayer(3, 2, IdentityActivator(), 0.001)
-    x, d = data_set()
-    rl.forward(x[0])
-    rl.forward(x[1])
+	error_function = lambda o: o.sum()
+	rl = RecurrentLayer(3, 2, IdentityActivator(), 0.001)
+	# x, d = data_set()
+	x = list()
+	x.append( [
+		[1],
+		[2],
+		[3]] )
+	x.append( [
+		[4],
+		[5],
+		[6] ] )
+	x.append( [
+		[7],
+		[8],
+		[9] ] )
+	rl.forward(x[0])
+	rl.forward(x[1])
+	# rl.forward(x[2])
 
-    sensitity_array = np.ones(rl.state_list[-1].shape,
-                              dtype=np.float64)
-    rl.backward(sensitity_array, IdentityActivator())
+	sensitity_array = np.ones(rl.state_list[-1].shape,
+							  dtype=np.float64)
+	print sensitity_array
+	rl.backward(sensitity_array, IdentityActivator())
+
+	# check gradient
+	epsilon = 10e-4
+	for i in range( rl.W.shape[ 0 ] ):
+		for j in range( rl.W.shape[ 1 ] ):
+			rl.W[i, j] += epsilon
+			rl.reset_state()
+			rl.forward( x[ 0 ] )
+			rl.forward( x[ 1 ] )
+			# rl.forward( x[ 2 ] )
+			err1 = error_function( rl.state_list[ -1 ] )
+			rl.W[ i, j ] -= 2*epsilon
+			rl.reset_state()
+			rl.forward( x[ 0 ] )
+			rl.forward( x[ 1 ] )
+			# rl.forward( x[ 2 ] )
+			err2 = error_function( rl.state_list[ -1 ] )
+			expect_grad = ( err1 - err2 ) / ( 2*epsilon )
+			rl.W[ i, j ] += epsilon
+			print 'weights(%d,%d): expected - actural %f - %f' % (
+					i, j, expect_grad, rl.gradient[ i, j] )

chap06/RecurrentLayer.py
@@ -2,63 +2,93 @@
 # -*- coding:utf8 -*-
 # Power by viekie. 2017-06-01 15:20:01
 import numpy as np
-import ElementWiseOp as ewop
+from ElementWiseOP import elements_wise_op as ewop
 
+import inspect
+
+def printFrame():
+	callerFrameRecord = inspect.stack()[1] # 0 represents this line
+											# 1 represents line at caller
+	frame = callerFrameRecord[ 0 ]
+	info = inspect.getframeinfo( frame )
+	print "__FILE__", info.filename
+	print "__FUNC__", info.function
+	print "__LINE__", info.lineno
 
 class RecurrentLayer(object):
-    def __init__(self, input_width, state_width, activator, learning_rate):
-        self.input_width = input_width
-        self.state_width = state_width
-        self.activator = activator
-        self.learning_rate = learning_rate
-        self.times = 0
-        self.state_list = []
-        self.state_list.append((np.zeros(state_width, 1)))
-        self.U = np.random.uniform((-0.0001, 0.0001,
-                                    (state_width, input_width)))
-        self.W = np.random.uniform((-0.0001, 0.0001,
-                                    (state_width, state_width)))
+	def __init__(self, input_width, state_width, activator, learning_rate):
+		self.input_width = input_width
+		self.state_width = state_width
+		self.activator = activator
+		self.learning_rate = learning_rate
+		self.times = 0
+		self.state_list = []
+		self.state_list.append((np.zeros((state_width, 1))))
+		#print self.state_list[ -1 ]
+		self.U = np.random.uniform(-0.0001, 0.0001,
+									(state_width, input_width))
+		#print self.U
+		self.W = np.random.uniform(-0.0001, 0.0001,
+									(state_width, state_width))
+		#print self.W
 
-    def forward(self, input_array):
-        self.times += 1
-        state = (np.dot(self.U, input_array) +
-                 np.dot(self.W, self.state_list[-1]))
-        ewop.element_wise_op(state, self.activator.forward)
-        self.state_list.append(state)
+	def forward(self, input_array):
+		self.times += 1
+		#print input_array
+		#print np.dot( self.U, input_array )
+		#print np.dot( self.W, self.state_list[ -1 ] )
+		#print "state"
+		state = (np.dot(self.U, input_array) +
+				 np.dot(self.W, self.state_list[-1]))
+		#print state
+		ewop.element_wise_op(state, self.activator.forward)
+		self.state_list.append(state)
 
-    def backward(self, sensitivity_map, activator):
-        self.calc_delta(sensitivity_map, activator)
-        self.calc_gradient()
+	def backward(self, sensitivity_map, activator):
+		self.calc_delta(sensitivity_map, activator)
+		self.calc_gradient()
 
-    def calc_delta(self, sensitivity_map, activator):
-        self.delta_list = []
-        for i in range(self.times):
-            self.delta_list.append(np.zeros(self.state_width, 1))
-        self.delta_list.append(sensitivity_map)
+	def calc_delta(self, sensitivity_map, activator):
+		self.delta_list = []
+		for i in range(self.times):
+			self.delta_list.append(np.zeros( (self.state_width, 1 )))
+		self.delta_list.append(sensitivity_map)
 
-        for k in range(self.times - 1, 0, -1):
-            self.calc_delta_k(k, activator)
+		for k in range(self.times - 1, 0, -1):
+			self.calc_delta_k(k, activator)
 
-    def calc_delta_k(self, k, activator):
-        state = self.state_list[k + 1].copy
-        ewop.element_wise_op(self.state_list[k + 1], activator.backward)
-        self.delta_list[k] = np.dot(np.dot(self.delta_list[k + 1].T,
-                                           self.W),
-                                    np.diag(state[:, 0])).T
+	def calc_delta_k(self, k, activator):
+		state = self.state_list[k + 1].copy()
 
-    def calc_gradient(self):
-        self.gradient_list = []
-        for t in range(self.times + 1):
-            self.gradient_list.append(
-                np.zeros((self.state_width, self.state_width)))
-        for t in range(self.times, 0, -1):
-            self.calc_gradient_t(t)
-        self.gradient = reduce(lambda a, b: a + b, self.gradient_list,
-                               self.gradient_list[0])
+		printFrame()
+		print state
 
-    def calc_gradient_t(self, t):
-        gradient = np.dot(self.delta_list[t], self.state_list[t - 1].T)
-        self.gradient_list[t] = gradient
+		# ewop.element_wise_op( self.state_list[k + 1], activator.backward )
+		ewop.element_wise_op( state, activator.backward )
+		print self.state_list[ k + 1 ]
+		print np.diag( state[ :, 0 ] )
+		self.delta_list[k] = np.dot(np.dot(self.delta_list[k + 1].T,
+										   self.W),
+									np.diag(state[:, 0])).T
 
-    def update(self):
-        self.W -= self.learning_rate * self.gradient
+	def calc_gradient(self):
+		self.gradient_list = []
+		for t in range(self.times + 1):
+			self.gradient_list.append(
+				np.zeros((self.state_width, self.state_width)))
+		for t in range(self.times, 0, -1):
+			self.calc_gradient_t(t)
+		self.gradient = reduce(lambda a, b: a + b, self.gradient_list,
+							   self.gradient_list[0])
+
+	def calc_gradient_t(self, t):
+		gradient = np.dot(self.delta_list[t], self.state_list[t - 1].T)
+		self.gradient_list[t] = gradient
+
+	def update(self):
+		self.W -= self.learning_rate * self.gradient
+	def reset_state(self):
+		self.times = 0       # 当前时刻初始化为t0
+		self.state_list = [] # 保存各个时刻的state
+		self.state_list.append(np.zeros(
+			(self.state_width, 1)))      # 初始化s0